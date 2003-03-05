db642f73276a0f5ae0afe648571ba4e119d153ae
plafoucr <plafoucr@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2003-03-05 10:28:02 +0000

    added : description, min, max and unit + getters & setters

--- fuzzy/Variable.py
+++ fuzzy/Variable.py
@@ -2,9 +2,13 @@ class Variable:
     """Base class for any kind of fuzzy variable.
        Returns as output the previous input value."""
 
-    def __init__(self):
+    def __init__(self,description='',min='',max='',unit=''):
         self.adjectives = {}
-        self.__value = None
+        self.__value     = None
+        self.description = description
+        self.min         = min
+        self.max         = max
+        self.unit        = unit
         
     def setValue(self,value):
         """Let adjectives calculate their membership values."""
@@ -15,6 +19,38 @@ class Variable:
     def getValue(self):
         """Return previous input value."""
         return self.__value
+
+    def setDescription(self,description):
+        """Set Variable description."""
+        self.description = description
+
+    def getDescription(self):
+        """Return Variable description."""
+        return self.description
+
+    def setMin(self,min):
+        """Set Variable minimum value."""
+        self.min = min
+
+    def getMin(self):
+        """Return Variable minimum."""
+        return self.min
+        
+    def setMax(self,max):
+        """Set Variable minimum value."""
+        self.max = max
+
+    def getMax(self):
+        """Return Variable minimum."""
+        return self.max
+    
+    def setUnit(self,unit):
+        """Set Variable unit."""
+        self.unit = unit
+
+    def getUnit(self):
+        """Return Variable unit."""
+        return self.unit
 
     def reset(self):
         """Reset meberships of adjectives for new calculation step."""
