e3a6802a1bcd77364a1a66fc8cae07cc16924191
rliebscher <rliebscher@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2003-03-20 08:19:45 +0000

    Added pydoc subclass which creates links to sf cvs web page

--- doc/makehtml.sh
+++ doc/makehtml.sh
@@ -5,10 +5,13 @@ cd html
 
 # if you want create local docs replace ../mypydoc.py 
 # with the path to the pydoc in your python lib directory
+#PYDOC=../mypydoc.py
+PYDOC=../sfpydoc.py
 
-python -O ../mypydoc.py -w fuzzy
-python -O ../mypydoc.py -w fuzzy.norm
-python -O ../mypydoc.py -w fuzzy.operator
-python -O ../mypydoc.py -w fuzzy.set
-python -O ../mypydoc.py -w ./../..
+
+python -O $PYDOC -w fuzzy
+python -O $PYDOC -w fuzzy.norm
+python -O $PYDOC -w fuzzy.operator
+python -O $PYDOC -w fuzzy.set
+python -O $PYDOC -w ./../..
 #cd ..
