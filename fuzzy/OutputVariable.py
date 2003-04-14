ddaee2272333f1510b5be9339e7cbe6c82f40d82
rliebscher <rliebscher@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2003-04-14 08:54:57 +0000

    added OutputVariable so you can use isinstance(X,OutputVariable)

---
+++ fuzzy/OutputVariable.py
@@ -0,0 +1,13 @@
+
+__revision__ = "$Id: OutputVariable.py,v 1.1 2003-04-14 08:54:57 rliebscher Exp $"
+
+
+from fuzzy.Variable import Variable
+
+
+class OutputVariable(Variable):
+    """marker class, so you can check if any variable is an (instance of) output variable 
+       """
+
+    def __init__(self,*args,**keywords):
+        Variable.__init__(*tuple([self]+list(args)),**keywords)
