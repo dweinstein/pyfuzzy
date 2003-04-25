9ae11bba35a91e1f243fd7c69b657078ae6626d6
rliebscher <rliebscher@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2003-04-25 07:37:54 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 # created 2001/08 Rene Liebscher
 
-__revision__ = "$Id: setup.py,v 1.2 2003-02-13 08:45:30 rliebscher Exp $"
+__revision__ = "$Id: setup.py,v 1.3 2003-04-25 07:37:54 rliebscher Exp $"
 
 from distutils.core import setup
 
 setup (name = "pyfuzzy",
-       version = "0.0.1",
+       version = "0.0.2",
        description = "Python Fuzzy Utilities",
        author = "Rene Liebscher",
        author_email = "R.Liebscher@gmx.de",
