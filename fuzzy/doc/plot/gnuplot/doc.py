e85cdf8a9e42632f83f2ad789aec1bac2dbf8f4b
rliebscher <rliebscher@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2008-11-30 20:20:54 +0000

    reworked plot code to work better with gnuplot_py added functions/methods to work with dict of Set instances added self.overscan to control how far we plot over the defined range (in % : 0.1 = 10%)

--- fuzzy/doc/plot/gnuplot/doc.py
+++ fuzzy/doc/plot/gnuplot/doc.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 """Plotting of variables, adjectives, ... using gnuplot"""
 
-__revision__ = "$Id: doc.py,v 1.2 2008-11-25 14:01:51 rliebscher Exp $"
+__revision__ = "$Id: doc.py,v 1.3 2008-11-30 20:20:54 rliebscher Exp $"
 
 
 def getMinMax(set):
@@ -19,12 +19,12 @@ def getMinMax(set):
 
     return (min,max)
 
-def getGlobalMinMax(variable):
-    """get tuple with minimum and maximum x-values used by the sets of this variable."""
+def getGlobalMinMax(sets):
+    """get tuple with minimum and maximum x-values used by the sets of this dicts of sets."""
     min = None
     max = None
-    for a in variable.adjectives.values():
-        (min2,max2) = getMinMax(a.set)
+    for s in sets.values():
+        (min2,max2) = getMinMax(s)
         if min is None or min2 < min:
             min = min2
         if max is None or max2 > max:
@@ -32,39 +32,31 @@ def getGlobalMinMax(variable):
     return (min,max)
 
 
-def getPoints(variable):
-    """Collect all important points of all adjectives in this variables."""
+def getPoints(sets):
+    """Collect all important points of all adjectives in this dict of sets."""
 
     from fuzzy.set.Set import merge
     # merge them all
     temp = None
-    for a in variable.adjectives.values():
+    for s in sets.values():
         if temp is None:
-            temp = a.set;
+            temp = s;
         else:
-            temp = merge(max,temp,a.set)
+            temp = merge(max,temp,s)
 
     # collect points
-    points = []
-    ig = temp.getIntervalGenerator()
-    next = ig.nextInterval(None,None)
-    prev = None
-    only_once = 0
-    while next is not None:
-        if next != prev: # avoid to have same value twice
-            points.append(next)
-            only_once = 0
-        else:
-            if only_once == 0:
-                points = points[:-1]
-                points.append(next-0.001) # not the best solution !!!!
-                points.append(next)
-                points.append(next+0.001) # not the best solution !!!!
-                only_once = 1
-        prev = next
-        next = ig.nextInterval(prev,None)
+    # >>> result of merge is always a Polygon object
+    points = [p[0] for p in temp.points]
+    # avoid to have same value twice (filter points out where successor is equal)
+    return points[:1] + [p0 for p0,p1 in zip(points[1:],points) if p0!=p1]
 
-    return points
+
+def getSets(variable):
+    """Get all sets of adjectives in this variable."""
+    sets = {}
+    for a_name,adj in variable.adjectives.items():
+        sets[a_name] = adj.set
+    return sets
 
 
 class Doc(object):
@@ -72,67 +64,40 @@ class Doc(object):
 
     def __init__(self,directory="doc"):
         self.directory=directory
+        self.overscan=0.1
 
     def initGnuplot2D(self,filename="plot",xlabel=None,ylabel=None,title=None,xrange_=None,yrange=None,x_logscale=0,y_logscale=0):
         import Gnuplot
         g = Gnuplot.Gnuplot(debug=0)
-        if xlabel:
-            g.xlabel(xlabel)
-        if ylabel:
-            g.ylabel(ylabel)
-        if title:
-            g.title(title)
-        if xrange_:
-            g('set xrange [%f:%f]' % xrange_)
-        else:
-            g('set autoscale x')
-        if yrange:
-            g('set yrange  [%f:%f]' % yrange)
-        else:
-            g('set autoscale y')
-        if x_logscale:
-            g('set logscale x')
-            g('set autoscale x')
-        if y_logscale:
-            g('set logscale y')
-            g('set autoscale y')
+        if xlabel is not None: g.xlabel(xlabel)
+        if ylabel is not None: g.ylabel(ylabel)
+        if title is not None: g.title(title)
+        if xrange_ is not None: g('set xrange [%f:%f]' % xrange_) 
+        else: g('set autoscale x')
+        if yrange is not None: g('set yrange  [%f:%f]' % yrange) 
+        else: g('set autoscale y')
+        if x_logscale: g('set logscale x'); g('set autoscale x')
+        if y_logscale: g('set logscale y'); g('set autoscale y')
         g("set terminal png small transparent truecolor nocrop")
         g("set output '%s/%s.png'" % (self.directory,filename))
-
         return g
 
     def initGnuplot3D(self,filename="plot3D",xlabel=None,ylabel=None,zlabel=None,title=None,xrange_=None,yrange=None,zrange=None,x_logscale=0,y_logscale=0,z_logscale=0):
         import Gnuplot
         g = Gnuplot.Gnuplot(debug=0)
-        if xlabel:
-            g.xlabel(xlabel)
-        if ylabel:
-            g.ylabel(ylabel)
-        if zlabel:
-            g("set zlabel '%s'" % zlabel)
-        if title:
-            g.title(title)
-        if xrange_:
-            g('set xrange [%f:%f]' % xrange_)
-        else:
-            g('set autoscale x')
-        if yrange:
-            g('set yrange  [%f:%f]' % yrange)
-        else:
-            g('set autoscale y')
-        if zrange:
-            g('set zrange  [%f:%f]' % zrange)
-        else:
-            g('set autoscale z')
-        if x_logscale:
-            g('set logscale x')
-            g('set autoscale x')
-        if y_logscale:
-            g('set logscale y')
-            g('set autoscale y')
-        if z_logscale:
-            g('set logscale z')
-            g('set autoscale z')
+        if xlabel is not None: g.xlabel(xlabel)
+        if ylabel is not None: g.ylabel(ylabel)
+        if zlabel is not None: g("set zlabel '%s'" % zlabel)
+        if title is not None:  g.title(title)
+        if xrange_ is not None: g('set xrange [%f:%f]' % xrange_) 
+        else: g('set autoscale x')
+        if yrange is not None: g('set yrange  [%f:%f]' % yrange) 
+        else: g('set autoscale y')
+        if zrange is not None: g('set zrange  [%f:%f]' % zrange) 
+        else: g('set autoscale z')
+        if x_logscale: g('set logscale x');g('set autoscale x')
+        if y_logscale: g('set logscale y');g('set autoscale y')
+        if z_logscale: g('set logscale z');g('set autoscale z')
         g("set terminal png small transparent truecolor nocrop")
         g("set output '%s/%s.png'" % (self.directory,filename))
         g('set style data lines')
@@ -142,16 +107,22 @@ class Doc(object):
         g('set contour surface')
         return g
 
+
     def getValues(self,v):
-        (min,max) = getGlobalMinMax(v)
+        return getValuesSets(getSets(v))
+
+
+    def getValuesSets(self,sets):
+        (min,max) = getGlobalMinMax(sets)
         width = max - min
-        min = min - 0.1 * width
-        max = max + 0.1 * width
+        min = min - self.overscan * width
+        max = max + self.overscan * width
         width = max - min
 
-        values = [min]+getPoints(v)+[max]
+        values = [min]+getPoints(sets)+[max]
 
         return (min,max,values)
+
 
     def createDoc(self,system):
         """create plots of all variables defined in the given system."""
@@ -170,14 +141,20 @@ class Doc(object):
     def createDocVariable(self,v,name,x_logscale=0,y_logscale=0):
         """Creates a 2D plot of a variable"""
 
+        self.createDocSets(getSets(v),name,x_logscale,y_logscale,description=v.description,units=v.unit)
+
+    def createDocSets(self,sets,name,x_logscale=0,y_logscale=0,description=None,units=None):
+        """Creates a 2D plot of dict of sets"""
+
         import Gnuplot
         import Gnuplot.funcutils
+        import fuzzy.set.Polygon
 
-        # sort adjectives by lowest x values
-        adjs = v.adjectives.keys()
+        # sort sets by lowest x values
+        set_keys = sets.keys()
         def cmp(a1,a2):
-            s1 = v.adjectives[a1].set
-            s2 = v.adjectives[a2].set
+            s1 = sets[a1]
+            s2 = sets[a2]
             x1 = s1.getIntervalGenerator().nextInterval(None,None)
             x2 = s2.getIntervalGenerator().nextInterval(None,None)
             # get lower x values
@@ -188,19 +165,24 @@ class Doc(object):
             # get higher membership value
             return int(s2(x2)-s1(x1))
 
-        adjs.sort(cmp)
+        set_keys.sort(cmp)
 
-        (min,max,x) = self.getValues(v)
+        (min,max,x) = self.getValuesSets(sets)
 
         # calculate values
         plot_items = []
-        for a_name in adjs:
-            a = v.adjectives[a_name]
-            plot_items.append(Gnuplot.funcutils.compute_Data(x, a.set, title=a_name, inline=1))
+        for s_name in set_keys:
+            s = sets[s_name]
+            if isinstance(s,fuzzy.set.Polygon.Polygon):
+                p = [(min,s(min))] + s.points + [(max,s(max))]
+                plot_item=Gnuplot.PlotItems.Data(p,title=s_name)
+            else:
+                plot_item=Gnuplot.funcutils.compute_Data(x, s, title=s_name)
+            plot_items.append(plot_item)
 
-        xlabel = v.description or ""
-        if v.unit is not None:
-            xlabel += " [%s]" % v.unit
+        xlabel = description or ""
+        if units is not None:
+            xlabel += " [%s]" % units
         g = self.initGnuplot2D(filename=name,xlabel=xlabel,ylabel="membership",title=name,xrange_=(min,max),yrange=(-0.2,1.2))
         g('set style fill transparent solid 0.5 border')
         g('set style data filledcurves y1=0')
@@ -231,7 +213,7 @@ class Doc(object):
 
         g = self.initGnuplot2D(filename=x_name+"_"+y_name,xlabel=x_name,ylabel=y_name,title=y_name+"=f("+x_name+")",xrange_=(x_min,x_max))
         g('set style data lines')
-        g.plot(Gnuplot.funcutils.compute_Data(x, f, inline=1))
+        g.plot(Gnuplot.funcutils.compute_Data(x, f))
         g("reset")
 
     def create3DPlot(self,system,x_name,y_name,z_name,input_dict={},output_dict={},x_logscale=0,y_logscale=0,z_logscale=0):
@@ -260,7 +242,7 @@ class Doc(object):
             return output_dict[z_name]
 
         g = self.initGnuplot3D(filename=x_name+"_"+y_name+"_"+z_name,xlabel=x_name,ylabel=y_name,zlabel=z_name,title="%s=f(%s,%s)" % (z_name,x_name,y_name),xrange_=(x_min,x_max),yrange=(y_min,y_max),x_logscale=x_logscale,y_logscale=y_logscale,z_logscale=z_logscale)
-        g.splot(Gnuplot.funcutils.compute_GridData(x,y, f,binary=0,inline=1))
+        g.splot(Gnuplot.funcutils.compute_GridData(x,y, f,binary=0))
         g("reset")
 
 
@@ -293,5 +275,5 @@ class Doc(object):
         g = self.initGnuplot3D(filename=x_name+"_"+y_name+"_"+z_name+"_"+adjective,xlabel=x_name,ylabel=y_name,zlabel=z_name,title="%s.%s=f(%s,%s)" % (z_name,adjective,x_name,y_name),xrange_=(x_min,x_max),yrange=(y_min,y_max),zrange=(0,1),x_logscale=x_logscale,y_logscale=y_logscale,z_logscale=z_logscale)
         g("set xyplane at 0")
         g("set cntrparam levels incremental 0.1,0.2,1.0")
-        g.splot(Gnuplot.funcutils.compute_GridData(x,y, f,binary=0,inline=1))
+        g.splot(Gnuplot.funcutils.compute_GridData(x,y, f,binary=0))
         g("reset")
