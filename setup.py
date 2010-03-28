2f0a26dade2bbdb5bce9b2fe89783e8bdacc9b9c
rliebscher <rliebscher@47a34fe4-acd1-4e77-8ce9-9ac14a26cd75>
2010-03-28 18:49:08 +0000

    some reformating

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 #
 # Copyright (C) 2009  Rene Liebscher
 #
@@ -10,10 +10,11 @@
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT 
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
 # 
-# You should have received a copy of the GNU Lesser General Public License along with 
-# this program; if not, see <http://www.gnu.org/licenses/>. 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>. 
 #
 """pyfuzzy: Python Fuzzy Utilities
 
@@ -21,7 +22,7 @@ pyfuzzy is a python module for working with fuzzy sets
 (for example for controllers or other similar stuff, 
 it can be also used for decision making in business.)
 """
-__revision__ = "$Id: setup.py,v 1.13 2009-10-07 21:08:14 rliebscher Exp $"
+__revision__ = "$Id: setup.py,v 1.14 2010-03-28 18:49:08 rliebscher Exp $"
 
 from distutils.core import setup
 
@@ -35,7 +36,7 @@ License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
 Programming Language :: Python
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Scientific/Engineering
-Operating System :: OS Independent
+Operating System :: OS Independent\
 """
 
 PACKAGES = [
@@ -55,29 +56,30 @@ PACKAGES = [
         ]
 
 try:
-    import antlr3
+    import antlr3 #@UnusedImport
     PACKAGES.extend([
             'fuzzy.storage.fcl',
             ])
 except:
-    print """\
+    import sys
+    sys.stdout.write("""\
 Sorry, without the python runtime of ANTLR3, there will be
 no reading of FCL files.
-"""
+""")
 
 if __name__ == "__main__":
     setup (name = "pyfuzzy",
-       version = "0.1.0",
+       version = "0.1.1",
        description = DOCLINES[0],
-       author = "Rene Liebscher",
+       author = "René Liebscher",
        author_email = "R.Liebscher@gmx.de",
-       maintainer = "Rene Liebscher",
+       maintainer = "René Liebscher",
        maintainer_email = 'R.Liebscher@gmx.de',
        url = "http://pyfuzzy.sourceforge.net",
        download_url = "http://sourceforge.net/project/showfiles.php?group_id=59160&package_id=55171",
        license = "LGPL+",
        long_description = "\n".join(DOCLINES[2:]),
-       classifiers=CLASSIFIERS.split('\n'),
+       classifiers = CLASSIFIERS.split('\n'),
        platforms = ["OS Independent"],
        packages = PACKAGES,
       )
